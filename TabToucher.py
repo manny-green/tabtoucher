2b2a9419ff4acf59e48a40e3e52781eae6fd068d
manny-green <eukpong06@gmail.com>
2024-02-08 01:26:14 -0500

    Added tab opening funtionality

--- TabToucher.py
+++ TabToucher.py
@@ -1,6 +1,5 @@
-import ctypes
 import time
-import math
+#import math
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -8,11 +7,16 @@ from selenium.webdriver.chrome.options import Options
 #This will initialize the WebDriver
 driver = webdriver.Chrome()
 
-#Open a webpage
+#Open a webpage and open up multiple tabs
 driver.get("https://www.google.com")
 
-#driver.refresh() 
-#driver.back()  
-#driver.forward() 
-#driver.quit()
+i = 0
+while i < 200:
+    driver.switch_to.new_window('tab') 
+    driver.get("https://giphy.com/gifs/math-lady-meme-WRQBXSCnEFJIuxktnw")
+    i += 1
+
+
+#Webdriver closes once the action of using driver.get is completed, or perhaps any action for that matter.
+time.sleep(1000)
 
