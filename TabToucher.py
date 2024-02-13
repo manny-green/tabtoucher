50aa99d359766708e2587872f032dfaf987dc240
manny-green <eukpong06@gmail.com>
2024-02-13 00:22:06 -0500

    Volume Fix

--- TabToucher.py
+++ TabToucher.py
@@ -12,29 +12,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-
 #This portion of code will lock the workstation
 import ctypes
-#lock_ws = ctypes.windll.user32.LockWorkStation()
+lock_ws = ctypes.windll.user32.LockWorkStation()
 
 #This portion of code focuses on importing Windows related modules
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-
 #This portion of code is for initializing windows audio functionality
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-
 #This reassigned the webdriver.Chrome() function to "driver"
 driver = webdriver.Chrome()
-
-
-
-
 
 #Open a webpage and open up multiple tabs
 driver.get("https://www.google.com")
@@ -42,24 +35,26 @@ driver.get("https://www.google.com")
 i = 0
 while i < 100:
     driver.switch_to.new_window('tab') 
-    driver.get("https://www.google.com")
-    volume.SetMasterVolumeLevelScalar(1.0, None)
-    time.sleep(0.5)
-    i += 1
+    driver.get("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    driver.get("https://www.youtube.com/watch?v=Y82jDHRrswc")
 
+    #The range here goes from -65.0 to 0. Anything higher than 0 or lower than -65.0 will produce an error.
+    if "macOS" in platform.platform():
+        osascript.osascript("set volume output volume 100")
+    elif "Windows" in platform.platform():
+        volume.SetMasterVolumeLevelScalar(1.0, None)
+
+    i += 1
 
 #This will check if macOS & Windows are found in the platform.platform(), if not the program will not run.
 #This will then check if macOS and Window is found in platform.platform(). 
 #If the statement is true, it will run the commands within the if statements.
-#if "macOS" in platform.platform():
-#        osascript.osascript("set volume output volume 100")
-
-#The range here goes from -65.0 to 0. Anything higher than 0 or lower than -65.0 will produce an error.
-
-
-
+    
+if "macOS" in platform.platform():
+      osascript.osascript("set volume output volume 100")
 
 #This will lock the workstation
 lock_ws()
+
 #Webdriver closes once the action of using driver.get is completed, or perhaps any action for that matter; so here we use time.sleep()
 time.sleep(1000)
