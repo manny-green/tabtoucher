3a8345a876ce055d50f5c6b8be53728bb1779da4
manny-green <eukpong06@gmail.com>
2024-02-15 01:30:41 -0500

    Finalized code

--- TabToucher.py
+++ TabToucher.py
@@ -4,36 +4,39 @@ import sys
 import comtypes
 import math
 import os
+
+
+
 if "macOS" in platform.platform():
     import osascript
+
+if "Windows" in platform.platform():
+
+    #This portion of code will lock the workstation
+    import ctypes
+    lock_ws = ctypes.windll.user32.LockWorkStation()
+
+    #This portion of code focuses on importing Windows related modules
+    from ctypes import cast, POINTER
+    from comtypes import CLSCTX_ALL
+    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
-#This portion of code will lock the workstation
-import ctypes
-lock_ws = ctypes.windll.user32.LockWorkStation()
-
-#This portion of code focuses on importing Windows related modules
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-
 #This portion of code is for initializing windows audio functionality
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-#This reassigned the webdriver.Chrome() function to "driver"
+#This reassigned the webdriver.Chrome() function to "driver" and will open a webpage and open up multiple tabs
 driver = webdriver.Chrome()
-
-#Open a webpage and open up multiple tabs
 driver.get("https://www.google.com")
 
 i = 0
 while i < 100:
+    
     driver.switch_to.new_window('tab') 
     driver.get("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     driver.get("https://www.youtube.com/watch?v=Y82jDHRrswc")
@@ -43,16 +46,13 @@ while i < 100:
         osascript.osascript("set volume output volume 100")
     elif "Windows" in platform.platform():
         volume.SetMasterVolumeLevelScalar(1.0, None)
-
+        
     i += 1
 
 #This will check if macOS & Windows are found in the platform.platform(), if not the program will not run.
 #This will then check if macOS and Window is found in platform.platform(). 
 #If the statement is true, it will run the commands within the if statements.
     
-if "macOS" in platform.platform():
-      osascript.osascript("set volume output volume 100")
-
 #This will lock the workstation
 lock_ws()
 
